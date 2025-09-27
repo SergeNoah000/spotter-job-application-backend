97351134afd2ee243a747c8332c417b691ee0ef1
SergeNoah000 <gaetan.noah@facsciences-uy1.cm>
2025-09-27 22:16:09 +0100

    add spotter-sergenoah-backend.onrender.com to allowed hosts

--- spotter_project/settings.py
+++ spotter_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-)5_)b#w65@vs4$n(&$mo%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')]) + [ "spotter-sergenoah-backend.onrender.com"]
 
 # Application definition
 DJANGO_APPS = [
