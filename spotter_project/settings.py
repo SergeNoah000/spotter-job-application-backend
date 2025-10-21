baff5c2b0dafaa90482977671d44c168feb10734
SergeNoah000 <gaetan.noah@facsciences-uy1.cm>
2025-10-21 23:22:04 +0100

    allow all origins

--- spotter_project/settings.py
+++ spotter_project/settings.py
@@ -206,7 +206,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', default=False, cast=bool)
+CORS_ALLOW_ALL_ORIGINS = True # config('CORS_ALLOW_ALL_ORIGINS', default=False, cast=bool)
 
 # Email Configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
